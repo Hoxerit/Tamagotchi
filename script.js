4106a06817eaf1d2ff4c4bec3acdf8c90614430e
netryxdev <adrianoneto2501@gmail.com>
2021-10-22 04:21:32 -0300

    Commit 1

---
+++ script.js
@@ -0,0 +1,14 @@
+var fome = 100;
+var higiene = 100;
+var sono = 100;
+var diversao = 100;
+var social = 100;
+
+function loop(time = 2){
+    var total = fome + higiene + sono + diversao + social;
+
+    if(fome <= 0 || higiene <= 0 || sono <= 0 || diversao <= 0 || social <= 0) {
+        document.getElementById('blocoImagem')
+    }
+
+}
