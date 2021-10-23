52c0722eab0633e8fe34e7aa9c9152e6de731b21
netryxdev <adrianoneto2501@gmail.com>
2021-10-23 04:02:13 -0300

    commit 2

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function loop(time = 2){
     var total = fome + higiene + sono + diversao + social;
 
     if(fome <= 0 || higiene <= 0 || sono <= 0 || diversao <= 0 || social <= 0) {
-        document.getElementById('blocoImagem')
+        document.getElementById('blocoImagem').innerHTML = 'Você deixou seu amadotchi morrer, tente novamente (atualize a página)'
     }
 
 }
